3edc09761e8f321773a3db963d84a0c1c6c2fecb
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-17 10:14:19 -0400

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
+import '@/styles/reset.css'
+import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+    return (
+        <Component {...pageProps} />
+    );
+};
